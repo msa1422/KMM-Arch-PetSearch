56eba4284707105b6df00abca7669ec1bd07727f
msa1422 <msaneakhtar@gmail.com>
2022-09-26 18:52:28 +0530

    Add Collapsible ImagePager to PetDetailScreen

--- petsearch-ios/petsearch-ios/Features/PetDetail/PetDetailScreen.swift
+++ petsearch-ios/petsearch-ios/Features/PetDetail/PetDetailScreen.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 import Shared
+import ScalingHeaderScrollView
 
 struct PetDetailScreen: View {
     
@@ -18,15 +19,17 @@ struct PetDetailScreen: View {
     @State private var state: PetDetailState?
     @State private var stateObserver: Closeable? = nil
     
+    @State private var scrollProgress: CGFloat = 0
+
     var body: some View {
         GeometryReader { proxy in
-            
-            ZStack(alignment: .topLeading) {
-                
-                ScrollView(.vertical, showsIndicators: false) {
+            ScalingHeaderScrollView {
+                ZStack(alignment: .topLeading) {
+                    Color.surface
+                        .edgesIgnoringSafeArea(.all)
+                        .zIndex(0)
                     
                     VStack(spacing: .zero) {
-                        
                         ImagePager(
                             imageWidth: proxy.size.width,
                             topInsetHeight: proxy.safeAreaInsets.top,
@@ -34,15 +37,17 @@ struct PetDetailScreen: View {
                         ) { image in
                             // OnClick on event for image
                         }
+                        .opacity(1 - scrollProgress)
                         
                         Divider()
+                            .opacity(1 - scrollProgress)
                         
                         // Pet Name
                         Text(state?.petInfo?.name ?? "")
                             .font(Font.titleMedium)
                             .foregroundColor(Color.onSurface)
                             .lineLimit(1)
-                            .padding(.init(top: 20, leading: 24, bottom: .zero, trailing: 24))
+                            .padding(.init(top: 16, leading: 24 + 36 * scrollProgress, bottom: .zero, trailing: 24))
                             .frame(maxWidth: .infinity, alignment: .leading)
                         
                         // Pet Description
@@ -51,98 +56,112 @@ struct PetDetailScreen: View {
                             .foregroundColor(Color.onSurface)
                             .lineLimit(2)
                             .opacity(0.75)
-                            .padding(.init(top: 4, leading: 24, bottom: 24, trailing: 24))
+                            .padding(.init(top: 4, leading: 24 + 36 * scrollProgress, bottom: 20, trailing: 24))
                             .frame(maxWidth: .infinity, alignment: .leading)
                         
                         Divider()
+                    }
+                    .zIndex(1)
+                    
+                    // Custom back button
+                    Button(action: { presentationMode.wrappedValue.dismiss() }) {
+                        Image(systemName: "chevron.left")
+                            .frame(width: 60, height: 60)
+                            .background(
+                                Rectangle()
+                                    .foregroundColor(Color.surface.opacity(0.75))
+                                    .cornerRadius(18, corners: [.topRight, .bottomRight])
+                            )
+                    }
+                    .zIndex(2)
+                    .padding(.init(top: proxy.safeAreaInsets.top + 6 + proxy.size.width * scrollProgress, leading: .zero, bottom: .zero, trailing: .zero))
+                }
+            } content: {
+                VStack {
+                    // Pet Description
+                    Text(state?.petInfo?.description_ ?? "")
+                        .font(Font.bodyMedium)
+                        .foregroundColor(Color.onSurface)
+                        .lineSpacing(1.4)
+                        .padding(.init(top: 20, leading: 24, bottom: .zero, trailing: 24))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    // Chracteristics grid
+                    if state?.petInfo?.tags?.isEmpty == false {
                         
-                        // Pet Description
-                        Text(state?.petInfo?.description_ ?? "")
-                            .font(Font.bodyMedium)
-                            .foregroundColor(Color.onSurface)
-                            .lineSpacing(1.4)
-                            .padding(.init(top: 20, leading: 24, bottom: .zero, trailing: 24))
-                            .frame(maxWidth: .infinity, alignment: .leading)
+                        SectionTitle(title: SharedR.strings().characteristics.desc().localized().uppercased())
                         
+                        LazyVGrid(
+                            columns: [GridItem(.flexible(), spacing: 2), GridItem(.flexible(), spacing: 2)],
+                            spacing: 2
+                        ) {
+                            ForEach(state?.petInfo?.tags ?? [], id: \.self) { tag in
+                                Text("• \(tag)")
+                                    .font(Font.bodyMedium)
+                                    .foregroundColor(Color.onSurface)
+                                    .padding(.init(top: 4, leading: 12, bottom: .zero, trailing: 12))
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                        }
+                        .padding(.init(top: .zero, leading: 12, bottom: .zero, trailing: 12))
+                    }
+                    
+                    // Attributes grid
+                    if let attrs = state?.petInfo?.attributes {
                         
-                        // Chracteristics grid
-                        if state?.petInfo?.tags?.isEmpty == false {
-                            
-                            SectionTitle(title: SharedR.strings().characteristics.desc().localized().uppercased())
-                            
-                            LazyVGrid(
-                                columns: [GridItem(.flexible(), spacing: 2), GridItem(.flexible(), spacing: 2)],
-                                spacing: 2
-                            ) {
-                                ForEach(state?.petInfo?.tags ?? [], id: \.self) { tag in
-                                    Text("• \(tag)")
+                        SectionTitle(title: SharedR.strings().attributes.desc().localized().uppercased())
+
+                        let yes = SharedR.strings().yes.desc().localized()
+                        let no = SharedR.strings().no.desc().localized()
+
+                        let attrNameList = [
+                            SharedR.strings().declawed.desc().localized().uppercased(),
+                            SharedR.strings().spay_neuter.desc().localized().uppercased(),
+                            SharedR.strings().spacial_needs.desc().localized().uppercased(),
+                            SharedR.strings().house_trained.desc().localized().uppercased(),
+                            SharedR.strings().vaccinated.desc().localized().uppercased(),
+                        ]
+                        let attrValueList = [
+                            attrs.declawed ? yes : no,
+                            attrs.spayedNeutered ? yes : no,
+                            attrs.specialNeeds ? yes : no,
+                            attrs.houseTrained ? yes : no,
+                            attrs.shotsCurrent ? yes : no,
+                        ]
+                        
+                        LazyVGrid(
+                            columns: [GridItem(.flexible(), spacing: 2), GridItem(.flexible(), spacing: 2)],
+                            spacing: 2
+                        ) {
+                            ForEach(Array(zip(attrNameList, attrValueList)), id: \.0) { attribute in
+                                
+                                VStack (alignment: .leading, spacing: 2) {
+                                    
+                                    Text(attribute.0)
+                                        .bold()
+                                        .font(Font.labelSmall)
+                                        .foregroundColor(Color.onSurface.opacity(0.62))
+                                    
+                                    Text(attribute.1)
                                         .font(Font.bodyMedium)
                                         .foregroundColor(Color.onSurface)
-                                        .padding(.init(top: 4, leading: 12, bottom: .zero, trailing: 12))
                                         .frame(maxWidth: .infinity, alignment: .leading)
                                 }
+                                .padding(.init(top: 4, leading: 12, bottom: 8, trailing: 12))
                             }
-                            .padding(.init(top: .zero, leading: 12, bottom: .zero, trailing: 12))
                         }
-                        
-
-                        // Attributes grid
-                        if let attrs = state?.petInfo?.attributes {
-                            
-                            SectionTitle(title: SharedR.strings().attributes.desc().localized().uppercased())
-
-                            let yes = SharedR.strings().yes.desc().localized()
-                            let no = SharedR.strings().no.desc().localized()
-
-                            // Create a map so that it can be used in LazyGrid
-                            let attrMap = [
-                                SharedR.strings().declawed.desc().localized().uppercased():  attrs.declawed ? yes : no,
-                                SharedR.strings().spay_neuter.desc().localized().uppercased():  attrs.spayedNeutered ? yes : no,
-                                SharedR.strings().spacial_needs.desc().localized().uppercased():  attrs.specialNeeds ? yes : no,
-                                SharedR.strings().house_trained.desc().localized().uppercased():  attrs.houseTrained ? yes : no,
-                                SharedR.strings().vaccinated.desc().localized().uppercased():  attrs.shotsCurrent ? yes : no
-                            ]
-                            
-                            LazyVGrid(
-                                columns: [GridItem(.flexible(), spacing: 2), GridItem(.flexible(), spacing: 2)],
-                                spacing: 2
-                            ) {
-                                ForEach(attrMap.keys.compactMap({ $0 }), id: \.self) { attribute in
-                                    
-                                    VStack (alignment: .leading, spacing: 2) {
-                                        
-                                        Text(attribute)
-                                            .bold()
-                                            .font(Font.labelSmall)
-                                            .foregroundColor(Color.onSurface.opacity(0.62))
-                                        
-                                        Text(attrMap[attribute] ?? no)
-                                            .font(Font.bodyMedium)
-                                            .foregroundColor(Color.onSurface)
-                                            .frame(maxWidth: .infinity, alignment: .leading)
-                                    }
-                                    .padding(.init(top: 4, leading: 12, bottom: 8, trailing: 12))
-                                }
-                            }
-                            .padding(.init(top: .zero, leading: 12, bottom: .zero, trailing: 12))
-                        }
-                        
-                        Spacer().frame(height: 72)
+                        .padding(.init(top: .zero, leading: 12, bottom: .zero, trailing: 12))
                     }
+                    
+                    Spacer().frame(height: 72)
                 }
-
-                // Custom back button
-                Button(action: { presentationMode.wrappedValue.dismiss() }) {
-                    Image(systemName: "chevron.left")
-                        .frame(width: 60, height: 60)
-                        .background(
-                            Rectangle()
-                                .foregroundColor(Color.white.opacity(0.62))
-                                .cornerRadius(18, corners: [.topRight, .bottomRight])
-                        )
-                }
-                .padding(.init(top: proxy.safeAreaInsets.top, leading: .zero, bottom: .zero, trailing: .zero))
             }
+            .height(
+                min: proxy.safeAreaInsets.top + 75,
+                max: proxy.safeAreaInsets.top + proxy.size.width + 75
+            )
+            .collapseProgress($scrollProgress)
+            .allowsHeaderGrowth()
             .background(Color.surface)
             .ignoresSafeArea()
             .navigationBarBackButtonHidden(true)
@@ -162,8 +181,6 @@ struct PetDetailScreen: View {
         }
     }
 }
-
-
 
 struct PetDetailScreen_Previews: PreviewProvider {
     static var previews: some View {
