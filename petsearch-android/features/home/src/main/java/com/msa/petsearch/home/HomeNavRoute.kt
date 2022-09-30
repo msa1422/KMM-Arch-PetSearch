a4234530f20b16be2e5620199edb72784378142f
msa1422 <msaneakhtar@gmail.com>
2022-09-30 22:45:54 +0530

    Add Compose Navigation animation

--- petsearch-android/features/home/src/main/java/com/msa/petsearch/home/HomeNavRoute.kt
+++ petsearch-android/features/home/src/main/java/com/msa/petsearch/home/HomeNavRoute.kt
@@ -1,7 +1,13 @@
 package com.msa.petsearch.home
 
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavBackStackEntry
+import com.msa.petsearch.commoncompose.AnimatedBackStack
 import com.msa.petsearch.commoncompose.NavRoute
 import com.msa.petsearch.shared.coreutil.sharedviewmodel.navigation.NavigationScreen
 import com.msa.petsearch.shared.domain.homeuicontract.HomeViewModel
@@ -19,4 +25,17 @@ object HomeNavRoute : NavRoute<HomeViewModel> {
     @Composable
     override fun viewModel(entry: NavBackStackEntry): HomeViewModel =
         koinViewModel(owner = { entry.viewModelStore })
+
+    override fun getPopEnterTransition(): (AnimatedBackStack.() -> EnterTransition?) = {
+        fadeIn(animationSpec = tween(durationMillis = 300))
+    }
+
+    override fun getPopExitTransition(): (AnimatedBackStack.() -> ExitTransition?) = {
+        // Basically a MaterialHold
+        fadeOut(
+            animationSpec = tween(durationMillis = 300),
+            targetAlpha = 1F
+        )
+    }
+
 }
