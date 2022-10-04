545d14f2ddebd59548fe31a28bb558bcb27a0d5c
msa1422 <msaneakhtar@gmail.com>
2022-10-05 00:05:36 +0530

    Move test dependencies to KMM plugin

--- petsearch-shared/domain/home/ui-contract/build.gradle.kts
+++ petsearch-shared/domain/home/ui-contract/build.gradle.kts
@@ -19,19 +19,3 @@ dependencies {
 
     commonMainImplementation(projects.petsearchShared.domain.home.dataSource)
 }
-
-tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
-    filter {
-        isFailOnNoMatchingTests = false
-    }
-    testLogging {
-        showExceptions = true
-        showStandardStreams = true
-        events = setOf(
-            org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
-            org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
-        )
-        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-    }
-}
