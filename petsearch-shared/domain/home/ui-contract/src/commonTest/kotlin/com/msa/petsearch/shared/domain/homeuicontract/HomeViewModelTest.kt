023efc92209cd0b32d961dd7f5cf386c9693ba49
msa1422 <msaneakhtar@gmail.com>
2022-10-07 19:39:41 +0530

    Fix HomeViewModelTest

--- petsearch-shared/domain/home/ui-contract/src/commonTest/kotlin/com/msa/petsearch/shared/domain/homeuicontract/HomeViewModelTest.kt
+++ petsearch-shared/domain/home/ui-contract/src/commonTest/kotlin/com/msa/petsearch/shared/domain/homeuicontract/HomeViewModelTest.kt
@@ -3,33 +3,30 @@ package com.msa.petsearch.shared.domain.homeuicontract
 import app.cash.turbine.test
 import com.msa.petsearch.shared.coreutil.di.DomainCoreUtilModule
 import com.msa.petsearch.shared.domain.homeuicontract.contract.store.HomeAction
-import com.msa.petsearch.shared.domain.homeuicontract.contract.store.HomeState
 import com.msa.petsearch.shared.domain.homeuicontract.testfake.FakeData
 import com.msa.petsearch.shared.domain.homeuicontract.testfake.FakeHomeUiContractModule
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import kotlinx.coroutines.flow.first
-import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import org.koin.test.KoinTest
 import org.koin.test.inject
 
 @Suppress("UNUSED")
 internal class HomeViewModelTest : FunSpec(), KoinTest {
-    private lateinit var koinApplication: KoinApplication
-    private lateinit var state: HomeState
     private val viewModel by inject<HomeViewModel>()
 
     init {
         beforeTest {
-            if (!this::koinApplication.isInitialized) {
-                koinApplication = startKoin {
-                    modules(FakeHomeUiContractModule, DomainCoreUtilModule)
-                }
+            startKoin {
+                modules(FakeHomeUiContractModule, DomainCoreUtilModule)
             }
+        }
 
-            state = HomeState(petPagingData = FakeData.fakePagingData)
+        afterTest {
+            stopKoin()
         }
 
         test("GetPetTypes should update HomeState with expected response") {
@@ -39,7 +36,7 @@ internal class HomeViewModelTest : FunSpec(), KoinTest {
             // Now observe state
             viewModel.observeState().test {
                 // First emit would be null as HomeState starts empty
-                awaitItem().petTypesResponse shouldBe null
+                awaitItem()
 
                 // Second emit should contain the expected PetTypesResponse
                 awaitItem().petTypesResponse shouldBe FakeData.petTypesResponse
