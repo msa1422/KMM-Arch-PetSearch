f21d2405c4edfb5fa2e99e702e0b44445db575d1
msa1422 <msaneakhtar@gmail.com>
2022-10-09 11:21:20 +0530

    Add HomeRepository tests

---
+++ petsearch-shared/data/repository/home/src/commonTest/kotlin/com/msa/petsearch/shared/repository/shared/testfake/FakePetFinderApi.kt
@@ -0,0 +1,21 @@
+package com.msa.petsearch.shared.repository.shared.testfake
+
+import com.msa.petsearch.shared.coreutil.resource.Resource
+import com.msa.petsearch.shared.coreutil.resource.asResource
+import com.msa.petsearch.shared.networkinfra.PetFinderApi
+import com.msa.petsearch.shared.networkinfra.response.PetTypesResponseDTO
+import com.msa.petsearch.shared.networkinfra.response.SearchPetResponseDTO
+
+internal class FakePetFinderApi : PetFinderApi {
+
+    override suspend fun <T> getPetTypes(mapper: PetTypesResponseDTO.() -> T): Resource<T> {
+        return FakePetTypesResponseDTO.asResource { mapper(it) }
+    }
+
+    override suspend fun <T> searchPets(
+        parameters: HashMap<String, Any?>?,
+        mapper: SearchPetResponseDTO.() -> T
+    ): Resource<T> {
+        return FakePetSearchResponseDTO.asResource { mapper(it) }
+    }
+}
---
+++ petsearch-shared/data/repository/home/src/commonTest/kotlin/com/msa/petsearch/shared/repository/shared/testfake/FakePetTypesResponse.kt
@@ -0,0 +1,45 @@
+package com.msa.petsearch.shared.repository.shared.testfake
+
+import com.msa.petsearch.shared.networkinfra.dto.PetTypeDTO
+import com.msa.petsearch.shared.networkinfra.response.PetTypesResponseDTO
+
+internal val FakePetTypesResponseDTO = PetTypesResponseDTO(
+    types = listOf(
+        PetTypeDTO(
+            name = "Dog",
+            coats = listOf("DogCoat0", "DogCoat1", "DogCoat2", "DogCoat3"),
+            colors = listOf(
+                "DogColor0",
+                "DogColor1",
+                "DogColor2",
+                "DogColor3",
+                "DogColor4"
+            ),
+            genders = listOf("Male", "Female")
+        ),
+        PetTypeDTO(
+            name = "Cat",
+            coats = listOf("CatCoat0", "CatCoat1", "CatCoat2", "CatCoat3"),
+            colors = listOf(
+                "CatColor0",
+                "CatColor1",
+                "CatColor2",
+                "CatColor3",
+                "CatColor4"
+            ),
+            genders = listOf("Male", "Female")
+        ),
+        PetTypeDTO(
+            name = "Horse",
+            coats = listOf("HorseCoat0", "HorseCoat1", "HorseCoat2", "HorseCoat3"),
+            colors = listOf(
+                "HorseColor0",
+                "HorseColor1",
+                "HorseColor2",
+                "HorseColor3",
+                "HorseColor4"
+            ),
+            genders = listOf("Male", "Female")
+        )
+    )
+)
