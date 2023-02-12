e5de653b56cd09e3ccb0d2a92d08ae0ef5949976
msa1422 <msaneakhtar@gmail.com>
2023-02-12 22:58:26 +0530

    Add PetDetailViewModel2

--- petsearch-shared/ui/petdetail/src/androidMain/kotlin/com/msa/petsearch/shared/ui/petdetail/di/SharedUiPetDetailPlatformModule.kt
+++ petsearch-shared/ui/petdetail/src/androidMain/kotlin/com/msa/petsearch/shared/ui/petdetail/di/SharedUiPetDetailPlatformModule.kt
@@ -1,9 +1,11 @@
 package com.msa.petsearch.shared.ui.petdetail.di
 
 import com.msa.petsearch.shared.ui.petdetail.PetDetailViewModel
+import com.msa.petsearch.shared.ui.petdetail.PetDetailViewModel2
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 internal actual val SharedUiPetDetailPlatformModule = module {
+    viewModelOf(::PetDetailViewModel2)
     viewModelOf(::PetDetailViewModel)
 }
