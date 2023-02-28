636f59fc8884d9bea7ffae3d628e5e48c06714fc
msa1422 <msaneakhtar@gmail.com>
2023-02-28 16:41:01 +0530

    Fix MessageDeque unreliable emits - Shared

--- petsearch-shared/core/util/src/commonMain/kotlin/com/msa/petsearch/shared/core/util/resource/MessageDeque.kt
+++ petsearch-shared/core/util/src/commonMain/kotlin/com/msa/petsearch/shared/core/util/resource/MessageDeque.kt
@@ -13,7 +13,22 @@ object MessageDeque : Queue<ResourceMessage> {
         get() = deque.size
 
     override suspend fun enqueue(element: ResourceMessage): Boolean {
-        return deque.none { it.id == element.id } && deque.add(element).also { peekAndUpdate() }
+        // If deque contains the element id return false,
+        // No need to take any action in this case
+        if (deque.any { it.id == element.id }) {
+            return false
+        }
+
+        // Add the element to the deque
+        val transaction = deque.add(element)
+
+        // If the element being enqueued is the only element in Deque, emit the message
+        // else wait for the Deque to be dequeued
+        if (count == 1) {
+            peekAndUpdate()
+        }
+
+        return transaction
     }
 
     override suspend fun dequeue(): ResourceMessage? {
--- petsearch-shared/core/util/src/commonTest/kotlin/com/msa/petsearch/shared/core/util/resource/MessageDequeTest.kt
+++ petsearch-shared/core/util/src/commonTest/kotlin/com/msa/petsearch/shared/core/util/resource/MessageDequeTest.kt
@@ -1,68 +1,76 @@
 package com.msa.petsearch.shared.core.util.resource
 
+import app.cash.turbine.test
 import com.msa.petsearch.shared.core.util.testfake.FakeResourceMessages
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.ints.shouldBeZero
-import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
 
 @Suppress("UNUSED")
 internal class MessageDequeTest : FunSpec({
-    lateinit var messageDeque: MessageDeque
+    test("GivenResourceMessage_MessageDeque_EnqueuesMessage") {
+        val messageDeque = MessageDeque
+        val stateMessage = FakeResourceMessages[0]
 
-    beforeTest {
-        messageDeque = MessageDeque(ArrayDeque())
-    }
+        messageDeque().test {
+            // Push the message
+            messageDeque.enqueue(stateMessage)
 
-    test("Should enqueue message") {
-        val expected = FakeResourceMessages[0]
-        messageDeque.enqueue(FakeResourceMessages[0])
-        messageDeque.readOnlyStateFlow().value shouldBe expected
-    }
+            // await emit
+            awaitItem() shouldBe stateMessage
 
-    test("Should perform Enqueue Dequeue and follow FIFO") {
-        // Check count and message value
-        messageDeque.count.shouldBeZero()
-        messageDeque.readOnlyStateFlow().value.shouldBeNull()
-
-        // Enqueue messages to the MessageDeque
-        messageDeque.enqueue(FakeResourceMessages[0])
-        messageDeque.enqueue(FakeResourceMessages[1])
-        messageDeque.enqueue(FakeResourceMessages[2])
-
-        // Check count and message value
-        messageDeque.count shouldBe 3
-        messageDeque.readOnlyStateFlow().value shouldBe FakeResourceMessages[0]
-
-        // Dequeue a message from the deque
-        messageDeque.dequeue()
-
-        // Check count and message value
-        messageDeque.count shouldBe 2
-        messageDeque.readOnlyStateFlow().value shouldBe FakeResourceMessages[1]
-
-        // Again dequeue a message form the deque
-        messageDeque.dequeue()
-
-        // Check count and message value
-        messageDeque.count shouldBe 1
-        messageDeque.readOnlyStateFlow().value shouldBe FakeResourceMessages[2]
-
-        // Again enqueue to deque,
-        // The first value should still be the one in the condition above
-        messageDeque.enqueue(FakeResourceMessages[3])
-
-        // Check count and message value
-        messageDeque.count shouldBe 2
-        messageDeque.readOnlyStateFlow().value shouldBe FakeResourceMessages[2]
-
-        // Dequeue all messages
-        repeat(messageDeque.count) {
+            // dequeue the message
             messageDeque.dequeue()
         }
+    }
 
-        // Check count and message value
-        messageDeque.count.shouldBeZero()
-        messageDeque.readOnlyStateFlow().value.shouldBeNull()
+    test("GivenSequenceOfMessages_MessageDeque_FollowsFirstInFirstOutRule") {
+        val messageDeque = MessageDeque
+        messageDeque().test {
+            // Check count and message value
+            messageDeque.count.shouldBeZero()
+
+            // Enqueue messages to the MessageDeque
+            messageDeque.enqueue(FakeResourceMessages[0])
+            messageDeque.enqueue(FakeResourceMessages[1])
+            messageDeque.enqueue(FakeResourceMessages[2])
+
+            // Check count and message value
+            messageDeque.count shouldBe 3
+            awaitItem() shouldBe FakeResourceMessages[0]
+
+            // Dequeue a message from the deque
+            messageDeque.dequeue()
+
+            // Check count and message value
+            messageDeque.count shouldBe 2
+            awaitItem() shouldBe FakeResourceMessages[1]
+
+            // Again dequeue a message form the deque
+            messageDeque.dequeue()
+
+            // Check count and message value
+            messageDeque.count shouldBe 1
+            awaitItem() shouldBe FakeResourceMessages[2]
+
+            // Again enqueue to deque,
+            // The first value should still be the one in the condition above
+            messageDeque.enqueue(FakeResourceMessages[3])
+
+            // Check count and message value
+            // No message shall be emitted because there is no dequeue of the last message
+            messageDeque.count shouldBe 2
+
+            // Dequeue all messages
+            repeat(messageDeque.count) {
+                messageDeque.dequeue()
+            }
+
+            // Last message should be emitted in the last dequeue loop
+            awaitItem() shouldBe FakeResourceMessages[3]
+
+            // Check count and message value
+            messageDeque.count.shouldBeZero()
+        }
     }
 })
