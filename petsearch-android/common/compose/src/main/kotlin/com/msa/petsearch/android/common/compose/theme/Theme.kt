80a181c8bac46a413ecc16ae62fc3a532043575e
msa1422 <msaneakhtar@gmail.com>
2023-08-30 08:51:33 +0530

    Minor optimizations in Compose Theme

--- petsearch-android/common/compose/src/main/kotlin/com/msa/petsearch/android/common/compose/theme/Theme.kt
+++ petsearch-android/common/compose/src/main/kotlin/com/msa/petsearch/android/common/compose/theme/Theme.kt
@@ -1,41 +1,49 @@
 package com.msa.petsearch.android.common.compose.theme
 
-import android.os.Build
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.M
+import android.os.Build.VERSION_CODES.O
 import android.os.Build.VERSION_CODES.Q
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 @Composable
-fun ApplicationTheme(content: @Composable () -> Unit) {
-    val view = LocalView.current
-    val window = (view.context as ComponentActivity).window
-    val windowsInsetsController = WindowCompat.getInsetsController(window, view)
-    val darkTheme = isSystemInDarkTheme()
-    val transparentNav = darkTheme || Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
-    val transparentStatus = darkTheme || Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
-    val colorTransparent = Color.Transparent.toArgb()
-    val colorBlackTranslucent = Color.Black.copy(alpha = 0.2F).toArgb()
-
-    WindowCompat.setDecorFitsSystemWindows(window, false)
-
-    window.statusBarColor = if (transparentStatus) colorTransparent else colorBlackTranslucent
-    window.navigationBarColor = if (transparentNav) colorTransparent else  colorBlackTranslucent
-
-    windowsInsetsController.isAppearanceLightStatusBars = !darkTheme
-    windowsInsetsController.isAppearanceLightNavigationBars = !darkTheme
-
-    if (Build.VERSION.SDK_INT >= Q) window.isNavigationBarContrastEnforced = false
-
+fun ApplicationTheme(
+    view: View = LocalView.current,
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     MaterialTheme(
         colorScheme = AppColorScheme,
         typography = AppTypography,
         shapes = AppShapes,
         content = content
     )
+
+    LaunchedEffect(darkTheme) {
+        val window = checkNotNull(view.context as? ComponentActivity).window
+        val transparent = Color.Transparent.toArgb()
+        val blackTranslucent = Color.Black.copy(alpha = 0.2F).toArgb()
+        val windowsInsetsController = WindowCompat.getInsetsController(window, view)
+
+        windowsInsetsController.isAppearanceLightStatusBars = !darkTheme
+        windowsInsetsController.isAppearanceLightNavigationBars = !darkTheme
+
+        window.statusBarColor = if (darkTheme || SDK_INT >= M) transparent else blackTranslucent
+        window.navigationBarColor = if (darkTheme || SDK_INT >= O) transparent else blackTranslucent
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        if (SDK_INT >= Q) {
+            window.isNavigationBarContrastEnforced = false
+        }
+    }
 }
